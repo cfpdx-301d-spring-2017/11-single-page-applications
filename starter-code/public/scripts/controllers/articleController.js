0ac8f7032e75fd4330d65f12a9084b8c41635196
Mark Petrie <petrie.mark@gmail.com>
2017-06-12 13:59:57 -0700

    Complete initial TODOs including updates to index.html, aboutController.js, articleController.js, routes.js and articleView.js

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -1,12 +1,17 @@
 'use strict';
 var app = app || {};
 
-(function(module) {
+(function (module) {
   const articleController = {};
   // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
+  articleController.init = function () {
+    $('main > section').hide();
+    $('#articles').show();
+    app.Article.fetchAll(app.articleView.initIndexPage);
 
+  }
 
   module.articleController = articleController;
 })(app);
