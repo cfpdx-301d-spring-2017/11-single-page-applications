13b5ec17de9d5ff6798058b97d5cddb1c71f3a6d
Joe Churchman <churchman.joe@gmail.com>
2017-06-12 13:23:14 -0700

    Completed TODOs on commited pages

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,6 +5,11 @@ var app = app || {};
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = () => {
+    // app.aboutView.init();
+    $( 'main > section' ).hide();
+    $( '#about' ).show();
+  }
 
 
   module.aboutController = aboutController;
